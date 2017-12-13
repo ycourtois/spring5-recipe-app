9a4a0af30b2d32bf9421284a9643b0234b99bf8a
Yann Courtois <ycourtois@ippon.fr>
2017-12-13 16:09:45 +0100

    add tests

---
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -0,0 +1,83 @@
+package guru.springframework.controllers;
+
+import guru.springframework.domain.Recipe;
+import guru.springframework.service.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.ui.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+/**
+ * @author yann.courtois@ippon.fr
+ * @since 12/13/2017
+ */
+public class IndexControllerTest {
+
+    @Mock
+    RecipeService recipeService;
+
+    @Mock
+    Model model;
+
+    IndexController indexController;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+
+        indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    public void testMockMVC() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+
+    @Test
+    public void getIndexPage() throws Exception {
+
+        //given
+        List<Recipe> recipes = new ArrayList<>();
+        recipes.add(new Recipe());
+
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+
+        recipes.add(recipe);
+
+        when(recipeService.getAllRecipes()).thenReturn(recipes);
+
+        ArgumentCaptor<List<Recipe>> argumentCaptor = ArgumentCaptor.forClass(List.class);
+
+        //when
+        String viewName = indexController.getIndexPage(model);
+
+
+        //then
+        assertEquals("index", viewName);
+        Mockito.verify(recipeService).getAllRecipes();
+        Mockito.verify(model).addAttribute(eq("recipes"), argumentCaptor.capture());
+        List<Recipe> expectedList = argumentCaptor.getValue();
+        assertEquals(2, expectedList.size());
+    }
+
+}
---
+++ src/test/java/guru/springframework/repository/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,38 @@
+package guru.springframework.repository;
+
+import guru.springframework.domain.UnitOfMeasure;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author yann.courtois@ippon.fr
+ * @since 12/13/2017
+ */
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UnitOfMeasureRepositoryIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @Test
+    public void findByDescription() {
+        final Optional<UnitOfMeasure> teaspoon = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        assertEquals("Teaspoon", teaspoon.get().getDescription());
+    }
+
+    @Test
+    public void findByDescription_2() {
+        final Optional<UnitOfMeasure> teaspoon = unitOfMeasureRepository.findByDescription("Cup");
+
+        assertEquals("Cup", teaspoon.get().getDescription());
+    }
+}
